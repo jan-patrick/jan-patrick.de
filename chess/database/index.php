bdbd95bf2bdcd9374847fcce8be249065c078493
jan-patrick <jan.schneider@hfg-gmuend.de>
2018-04-21 12:55:29 +0200

    chess file system added

--- chess/database/index.php
+++ chess/database/index.php
@@ -1,23 +1,13 @@
 <?php
-require_once "data.php";
+include("../connect.php");
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// Connect to the local MySQL-Database and return the DB-Connection-Object
+$link = Connection();
+// String for the MySQL-Query: Here we select all of the table "tempLog"
+$query = "SELECT * FROM `test` ORDER BY `id` DESC";
 
-$sql = 'SELECT * 
-		FROM test 
-		ORDER BY id
-         ';
-		
-$query = mysqli_query($conn, $sql);
-
-if (!$query) {
-	die ('SQL Error: ' . mysqli_error($conn));
-}
+// Execute the MySQL-Query: Select all of the table "tempLog"
+$result = mysqli_query($link, $query);
 ?>
 
 <html>
@@ -25,10 +15,10 @@ if (!$query) {
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>DATABASE - Jan Schneider</title>
-<link rel="shortcut icon" href="../img/logo.png">
-<link rel="icon" type="imgage/png" href="../img/logo.svg">
-<link rel="image_src" href="../img/logo.svg" />
-<link rel="apple-touch-icon" href="../img/logo.png" />
+<link rel="shortcut icon" href="../../img/logo.png">
+<link rel="icon" type="imgage/png" href="../../img/logo.svg">
+<link rel="image_src" href="../img/../logo.svg" />
+<link rel="apple-touch-icon" href="../img/../logo.png" />
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700" rel="stylesheet">
 <meta name="description" content="The CV of Jan Schneider, currently IoT student at HfG Schwäbisch Gmünd"> 
 <meta name="keywords" content="Jan Schneider, HfG, IoT, Internet der Dinge, Student, Hochschule für Gestaltung, Schwäbisch Gmünd, Gmünd, Internet of Things, Bachelor of Arts, Kikife, Kulturbüro, FSJ, Jan-Patrick, Jan, Schneider">
@@ -36,7 +26,7 @@ if (!$query) {
 <head>
 </head>
 <body>
-<p><a href="https://www.jan-patrick.de/test">to TEST page</a></p>
+<p><a href="https://www.jan-patrick.de/chess">to CHESS page</a></p>
 <h1>test turn database</h1>
 	<table class="data-table">
 		<caption class="title">ESP sends test data</caption>
