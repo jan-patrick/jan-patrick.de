c9f03e5022a1d9a0b6fdcf243c4ff3afe50e7380
jan-patrick <jan.schneider@hfg-gmuend.de>
2018-06-15 20:17:59 +0200

    about-img update

--- js/custom.js
+++ js/custom.js
@@ -112,7 +112,7 @@ $(document).ready(function () {
         // INJECT CSS
         var css = document.createElement("style");
         css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #9E99C0}";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #525153}";
         document.body.appendChild(css);
     };
 
