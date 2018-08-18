d0fb84913210080fe3b14f282d188d4c6c6c5635
jan-patrick <jan.schneider@hfg-gmuend.de>
2018-08-18 12:57:26 +0200

    lostBrick as real project site

--- js/custom.js
+++ js/custom.js
@@ -158,7 +158,6 @@ $(document).ready(function () {
 
     //animatedModal
     $("#demo01").animatedModal();
-    $("#demo02").animatedModal2();
     $("#demo03").animatedModal3();
     $("#demo04").animatedModal4();
     $("#demo06").animatedModal6();
